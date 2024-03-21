b08ac316d0569adefa1b1191d8ac7d08c0038e71
emju204c <emil.jung@mailbox.tu-dresden.de>
2024-03-21 18:28:51 +0100

    tested it, not really working, probably too simple

--- src/movement.py
+++ src/movement.py
@@ -13,16 +13,25 @@ def following_line():
     # action before a motor changes speed
 
     # function for changing speed
-    def change_speed(side, speed):
+    def change_speed(side, speed, t):
         if side == "l":
             # speed for the motors on a sale 0 - 100
             m_left.duty_cycle_sp = speed
             m_left.command = "run-direct"
-            time.sleep(1)
+            time.sleep(t)
         elif side == "r":
             m_right.duty_cycle_sp = speed
             m_right.command = "run-direct"
-            time.sleep(1)
+            time.sleep(t)
+
+    # changing speed simultaneously
+    def speed(v, t):
+        motor_prep()
+        m_left.duty_cycle_sp = v
+        m_right.duty_cycle_sp = v
+        m_left.command = "run-direct"
+        m_right.command = "run-direct"
+        time.sleep(t)
 
     # function for preparating the motors
     def motor_prep():
@@ -42,24 +51,36 @@ def following_line():
         cs.mode = 'RGB-RAW'
         # print(cs.raw)
         c = cs.raw
-        # for using the large motors
         # right motor is on output C
         m_right = ev3.LargeMotor("outC")
         # left motor is on output A
         m_left = ev3.LargeMotor("outA")
-
-        if c[0] < 50 and c[1] < 80 and c[2] < 30:  # for the case the colour is black
-            motor_prep()
-            change_speed("l", 40)
-            change_speed("r", 40)
-            break
-        elif c[0] > 250 and c[1] > 260 and c[2] > 180:  # for the case the color is white
+        print("checking for colours", c)
+        if c[0] < 100 and c[1] < 130 and c[2] < 100:  # for the case the colour is black
+            print("its black")
+            speed(20, 2)
+        elif c[0] > 220 and c[1] > 300 and c[2] > 150:  # for the case the color is white
             # to check later if its getting more white after correction => means wrong direction
+            print("its white")
             check = cs.raw
-            motor_prep()
-            change_speed("l", 20)
-            change_speed("r", 40)
-            break
+            # turning left
+            change_speed("l", 15, 1)
+            change_speed("r", 20, 1)
+            print("turning left")
+            # getting new colour for checking
+            c = cs.raw
+            if c[0] > check[0] and c[1] > check[1] and c[2] > check[2]:
+                # direction correction
+                print("correction")
+                # turning right
+                change_speed("l", 20, 2)
+                change_speed("r", 16, 2)
+                # trying to get it leveled
+                change_speed("l", 20, 1)
+                change_speed("r", 24, 1)
+        else:
+            print("no white and no black")
+            speed(30, 1)
 
         # blau: (32, 123, 93)
         # weiß: (267, 381, 197)
