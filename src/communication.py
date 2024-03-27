f6b47b3d6aa9cf9c68504ba9b8d5d7788b3dbcd8
mikr037f <milan_halvar.krueger@mailbox.tu-dresden.de>
2024-03-27 23:42:38 +0100

    Add message handling and parsing

--- src/communication.py
+++ src/communication.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 # Attention: Do not import the ev3dev.ev3 module in this file
-import logging
 import ssl
 from collections.abc import Callable, Mapping
-from enum import StrEnum
+from dataclasses import dataclass
+from enum import StrEnum, unique
 from json import dumps, loads
 from logging import Logger
 from typing import Any, Final
 
 from paho.mqtt.client import Client, MQTTMessage
+
+from planet import Direction
 
 
 # The MQTT quality of service used for all publishing/subscribing.
@@ -27,8 +29,9 @@ TOPIC_EXPLORER: Final = "explorer/" + GROUP_ID
 TOPIC_PLANET_TEMPLATE: Final = "planet/{}/" + GROUP_ID
 
 
+@unique
 class MessageTypes(StrEnum):
-    """The types of messages that can be sent or received."""
+    """The types of messages that are recognized when sent or received."""
     READY = "ready"
     PLANET = "planet"
     PATH = "path"
@@ -40,6 +43,78 @@ class MessageTypes(StrEnum):
     DONE = "done"
 
 
+class PathStatus(StrEnum):
+    """The possible values for the path status."""
+    BLOCKED = "blocked"
+    FREE = "free"
+
+
+# An inheritable `typing.NamedTuple` alternative.
+# NOTE: Cannot enable `slots` parameter as it prohibits multiple inheritance
+#       (or at least makes it less usable).
+FrozenDataClass = dataclass(frozen=True)
+
+
+@FrozenDataClass
+class StartCoordinatesRecord:
+    """Starting coordinates."""
+    startX: int
+    startY: int
+
+
+@FrozenDataClass
+class DirectionRecord:
+    """Starting direction."""
+    # Not a base class of `StartRecord` in order to retain field ordering.
+    startDirection: Direction
+
+
+@FrozenDataClass
+class StartRecord(DirectionRecord, StartCoordinatesRecord):
+    """Conjunction of start coordinates and direction."""
+
+
+@FrozenDataClass
+class EndRecord:
+    """Ending coordinates and direction."""
+    endX: int
+    endY: int
+    endDirection: Direction
+
+
+@FrozenDataClass
+class PlanetRecord(StartCoordinatesRecord):
+    """Initial planet information."""
+    planetName: str
+    # Attention: Not `startDirection`!
+    startOrientation: Direction
+
+
+@FrozenDataClass
+class PathRecord(EndRecord, StartRecord):
+    """Path data."""
+    pathStatus: PathStatus
+
+
+@FrozenDataClass
+class WeightedPathRecord(PathRecord):
+    """Path data including weight."""
+    pathWeight: int
+
+
+@FrozenDataClass
+class MessageRecord:
+    """Message data."""
+    message: str
+
+
+@FrozenDataClass
+class TargetRecord:
+    """Target coordinates."""
+    targetX: int
+    targetY: int
+
+
 class Communication:
     """MQTT communication client for a planet discovery robot."""
 
@@ -47,9 +122,9 @@ class Communication:
     def __init__(self, mqtt_client: Client, logger: Logger):
         """Initialize communication, connect to server, subscribe."""
         # DO NOT CHANGE THE SETUP HERE
-        self.client = mqtt_client
-        self.client.tls_set(tls_version=ssl.PROTOCOL_TLS)
-        self.client.on_message = self.safe_on_message_handler
+        self._client = mqtt_client
+        self._client.tls_set(tls_version=ssl.PROTOCOL_TLS)
+        self._client.on_message = self.safe_on_message_handler
 
         # ==============================================================
 
@@ -57,21 +132,21 @@ class Communication:
         # executed when the specified message is received.
         self.message_type_handlers: Mapping[MessageTypes, Callable[..., Any]] = {}
 
-        self.client.enable_logger()    # DEBUG
+        self._client.enable_logger()    # DEBUG
 
-        self.client.username_pw_set(GROUP_ID, password=PASSWORD)
-        self.client.connect(HOST, port=PORT)
+        self._client.username_pw_set(GROUP_ID, password=PASSWORD)
+        self._client.connect(HOST, port=PORT)
 
-        self.client.subscribe(TOPIC_EXPLORER, qos=QOS)
-        self.client.loop_start()
+        self._client.subscribe(TOPIC_EXPLORER, qos=QOS)
+        self._client.loop_start()
 
-        self.logger = logger
+        self._logger = logger
 
     # DO NOT EDIT THE METHOD SIGNATURE
     def on_message(self, client: Client, data: Any, message: MQTTMessage):
         """Callback to handle if any message arrived."""
         payload = loads(message.payload.decode("utf-8"))
-        self.logger.debug(dumps(payload, indent=2))
+        self._logger.debug(dumps(payload, indent=2))
         if payload["from"] != "server":
             # Ignore any non server message (to not reprocess sent messages).
             return
@@ -94,25 +169,27 @@ class Communication:
             )(payload["payload"])
         )
 
-    def _handle_planet_message(self, payload: Any):
+    def _handle_planet_message(self, payload: Any) -> PlanetRecord:
         # Ready message sent, now receiving planet name.
         self._topic_planet = TOPIC_PLANET_TEMPLATE.format(payload["planetName"])
-        self.client.subscribe(self._topic_planet, QOS)
+        self._client.subscribe(self._topic_planet, QOS)
 
-    def _handle_path_message(self, payload: Any):
-        ...
+        return PlanetRecord(**payload)
 
-    def _handle_path_unveiled_message(self, payload: Any):
-        ...
+    def _handle_path_message(self, payload: Any) -> WeightedPathRecord:
+        return WeightedPathRecord(**payload)
 
-    def _handle_path_select_message(self, payload: Any):
-        ...
+    def _handle_path_unveiled_message(self, payload: Any) -> WeightedPathRecord:
+        return WeightedPathRecord(**payload)
 
-    def _handle_target_message(self, payload: Any):
-        ...
+    def _handle_path_select_message(self, payload: Any) -> DirectionRecord:
+        return DirectionRecord(**payload)
 
-    def _handle_done_message(self, payload: Any):
-        ...
+    def _handle_target_message(self, payload: Any) -> TargetRecord:
+        return TargetRecord(**payload)
+
+    def _handle_done_message(self, payload: Any) -> MessageRecord:
+        return MessageRecord(**payload)
 
     # DO NOT EDIT THE METHOD SIGNATURE
     #
@@ -120,10 +197,10 @@ class Communication:
     # string and an already encoded JSON-Object as message.
     def send_message(self, topic: str, message: Any):
         """Sends given message to specified channel."""
-        self.logger.debug("Publishing to: " + topic)
-        self.logger.debug(dumps(message, indent=2))
+        self._logger.debug("Publishing to: " + topic)
+        self._logger.debug(dumps(message, indent=2))
 
-        self.client.publish(topic, dumps(message), QOS)
+        self._client.publish(topic, dumps(message), QOS)
 
     # DO NOT EDIT THE METHOD SIGNATURE OR BODY
     #
